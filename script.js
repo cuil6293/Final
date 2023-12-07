08099fb57f0112b1e441d33590d7a01c902417ea
Cui <cl6293@nyu.edu>
2023-12-07 13:43:00 -0500

    Added Files

---
+++ script.js
@@ -0,0 +1,18 @@
+let peopleDatabase = [
+{
+	"image": "People1.png",
+	"name": "Bethany Martin, 26"
+},
+{
+	"image": "People2.png",
+	"name": "Joe Hall, 32"
+},
+{
+	"image": "People3.png",
+	"name": "Wendy Zhao, 23"
+},
+{
+	"image": "People4.png",
+	"name": "Brandon Lee, 22"
+}
+]
